66daad5eec75bfbafa6592e46a1b2c27c35f3bf3
Lê Thị Hồng Nhung <lethihongnhung@MacBook-Pro-cua-Nhung.local>
2020-11-18 23:49:35 +0700

    update n exit q exit()

--- phan_tich_so.py
+++ phan_tich_so.py
@@ -1,7 +1,9 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 excel_file = 'data.xlsx'
 data = pd.read_excel(excel_file, index = False).values
+x =pd.read_excel(excel_file, index = False).values
 
 def dataSort(data):
     lenght = len(data)
@@ -45,26 +47,26 @@ def ConsiderApprox(data, indexs, y):
     Newton_lui = []
     Lagrange = []
     while lenght >= 0:
-        if data[indexs[lenght][0]][1] > data[indexs[lenght][1]][1]:
-            if (data[indexs[lenght][0]][1]>y or y>data[indexs[lenght][1]][1]):
+        if data[indexs[lenght][0]][1] > data[indexs[lenght][1]][1]: 
+            # Xét trên đơn điệu giảm
+            if (data[indexs[lenght][0]][1]<y or y<data[indexs[lenght][1]][1]):
                 indexs.remove(indexs[lenght])
             else:
-                # tinh chat can xet trong doan
-                # tim va dua ra moc noi suy  
-                 
-                pass
+                print(-data)
+                Lagrange_Newton(-data, indexs[lenght], y)
         else:
+            # xét trên đơn điệu tăng
             if data[indexs[lenght][0]][1]>y or y>data[indexs[lenght][1]][1]:
                 indexs.remove(indexs[lenght])
             else:
-                # tinh chat can xet trong doan
-                # tim va dua va moc noi suy
-                pass
+                Lagrange_Newton(data, indexs[lenght], y)
         lenght -= 1
     
     return indexs
 
+# Đơn điệu tăng
 def Lagrange_Newton(data, index, y):
+    delta_y = []
     if index[1] - index[0] == 1:
         print("Newton")
     else:
@@ -73,16 +75,21 @@ def Lagrange_Newton(data, index, y):
         elif data[index[1]-1][1] < y and data[index[1]][1] > y:
             print("Newton Lui")
         else:
-            
-
-            if ...:
-                # 
+            for i in range(index[1]-index[0]):
+                delta = abs(data[index[0]+i][1] - data[index[0]+i+1][1])
+                print(data[index[0]+i][1], data[index[0]+i+1][1])
+                print(delta)
+                delta_y.append(delta)
+            if (delta_y[1]/delta_y[0] <= 1.5 and delta_y[1]/delta_y[0] >= 0.67) or (delta_y[-1]/delta_y[-2] <= 1.5 and delta_y[-1]/delta_y[-2] >= 0.67):
+                print
+                print('Larange')
             else:
-                # 
-    return
-
+                print('Newton')
 
 if __name__ == "__main__":
+    # sap xep lai bo du lieu tang dan theo x
+    # kiem tra dieu kien noi suy
+    # xet tung khoang (L or N)
     data_sort = dataSort(data)
     print(data_sort)
     print(InterpolationConditions(data_sort))
@@ -91,3 +98,5 @@ if __name__ == "__main__":
     y = 2.5
     index_y = ConsiderApprox(data, indexs, y)
     print(index_y)
+    plt.plot([0,0.2,0.4,0.6,0.8,1,1.2], [1.27,2.14,3.01,3.86,3.45,2.05,1.75] ,'go-')
+    plt.show()
