f3aa7695ba6344b409049b0b630c8fe295aa5f3d
Lê Thị Hồng Nhung <lethihongnhung@MacBook-Pro-cua-Nhung.local>
2020-11-18 22:50:02 +0700

    update

--- phan_tich_so.py
+++ phan_tich_so.py
@@ -1,12 +1,3 @@
-# input: mang du lieu(x,y), diem y
-# doc file thanh array 2D
-# sap xep lai theo thu tu tang dan cua x(viet ham)
-# tim khoang don dieu
-# tim khoang don dien chua y, xet tinh chat trong khoang ==> lua chon phuong phap
-# uu nhuoc diem cua 2 pp deu lua chon pp la tot nhat.?????
-# output: Khoang(phuong phap giai quyet) + moc noi suy
-
-
 import pandas as pd 
 
 excel_file = 'data.xlsx'
@@ -50,25 +41,46 @@ def InterpolationConditions(data):
 
 def ConsiderApprox(data, indexs, y):
     lenght = len(indexs) - 1
-
+    Newton_tien = []
+    Newton_lui = []
+    Lagrange = []
     while lenght >= 0:
         if data[indexs[lenght][0]][1] > data[indexs[lenght][1]][1]:
             if (data[indexs[lenght][0]][1]>y or y>data[indexs[lenght][1]][1]):
                 indexs.remove(indexs[lenght])
             else:
                 # tinh chat can xet trong doan
-                # tim va dua ra moc noi suy
-                break
+                # tim va dua ra moc noi suy  
+                 
+                pass
         else:
             if data[indexs[lenght][0]][1]>y or y>data[indexs[lenght][1]][1]:
                 indexs.remove(indexs[lenght])
             else:
-                #tinh chat can xet trong doan
+                # tinh chat can xet trong doan
                 # tim va dua va moc noi suy
-                break
+                pass
         lenght -= 1
     
     return indexs
+
+def Lagrange_Newton(data, index, y):
+    if index[1] - index[0] == 1:
+        print("Newton")
+    else:
+        if data[index[0]+1][1] > y and data[index[0]][1] < y:
+            print("Newton Tien")
+        elif data[index[1]-1][1] < y and data[index[1]][1] > y:
+            print("Newton Lui")
+        else:
+            
+
+            if ...:
+                # 
+            else:
+                # 
+    return
+
 
 if __name__ == "__main__":
     data_sort = dataSort(data)
@@ -79,9 +91,3 @@ if __name__ == "__main__":
     y = 2.5
     index_y = ConsiderApprox(data, indexs, y)
     print(index_y)
-
-# print(10%8)
-
-# s = 0
-# for i in range(1,101):
-#     for i 
